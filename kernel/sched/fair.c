2ec729d2e7fae95d93020d34e154514736f4152a
ShivamKumarJha <jha.shivam3@gmail.com>
2021-03-30 23:19:25 +0530

    Add kernel/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -994,7 +994,6 @@ update_stats_enqueue_sleeper(struct cfs_rq *cfs_rq, struct sched_entity *se)
 			}
 
 			trace_sched_stat_blocked(tsk, delta);
-			trace_sched_blocked_reason(tsk);
 
 			/*
 			 * Blocking time is in units of nanosecs, so shift by
@@ -6573,28 +6572,17 @@ static void walt_find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 	int prev_cpu = task_cpu(p);
 	int unisolated_candidate = -1;
 	int order_index = fbt_env->order_index, end_index = fbt_env->end_index;
-	int stop_index = INT_MAX;
 	int cluster;
 	unsigned int target_nr_rtg_high_prio = UINT_MAX;
 	bool rtg_high_prio_task = task_rtg_high_prio(p);
 	cpumask_t visit_cpus;
+	bool io_task_pack = (order_index > 0 && p->in_iowait);
 
 	/* Find start CPU based on boost value */
 	start_cpu = fbt_env->start_cpu;
 
-	/*
-	 * For higher capacity worth I/O tasks, stop the search
-	 * at the end of higher capacity cluster(s).
-	 */
-	if (order_index > 0 && p->wts.iowaited) {
-		stop_index = num_sched_clusters - 2;
-		most_spare_wake_cap = LONG_MIN;
-	}
-
-	if (fbt_env->strict_max) {
-		stop_index = 0;
-		most_spare_wake_cap = LONG_MIN;
-	}
+	if (fbt_env->strict_max || io_task_pack)
+		target_max_spare_cap = LONG_MIN;
 
 	if (p->state == TASK_RUNNING)
 		most_spare_wake_cap = ULONG_MAX;
@@ -6658,8 +6646,9 @@ static void walt_find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 				most_spare_cap_cpu = i;
 			}
 
-			if (per_task_boost(cpu_rq(i)->curr) ==
-					TASK_BOOST_STRICT_MAX)
+			if ((per_task_boost(cpu_rq(i)->curr) ==
+					TASK_BOOST_STRICT_MAX) &&
+					!fbt_env->strict_max)
 				continue;
 			/*
 			 * Cumulative demand may already be accounting for the
@@ -6678,7 +6667,8 @@ static void walt_find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 			 * than the one required to boost the task.
 			 */
 			new_util = max(min_util, new_util);
-			if (new_util > capacity_orig)
+			if (!(fbt_env->strict_max || io_task_pack) &&
+					new_util > capacity_orig)
 				continue;
 
 			/*
@@ -6768,9 +6758,6 @@ static void walt_find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 
 		if ((cluster >= end_index) && (target_cpu != -1) &&
 			walt_target_ok(target_cpu, order_index))
-			break;
-
-		if (most_spare_cap_cpu != -1 && cluster >= stop_index)
 			break;
 	}
 
@@ -7221,9 +7208,7 @@ int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu,
 				continue;
 			util = cpu_util_next(cpu, p, cpu);
 			cpu_cap = capacity_of(cpu);
-			spare_cap = cpu_cap;
-			lsub_positive(&spare_cap, util);
-
+			spare_cap = cpu_cap - util;
 			/*
 			 * Skip CPUs that cannot satisfy the capacity request.
 			 * IOW, placing the task there would make the CPU
@@ -8165,11 +8150,9 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 	if (!can_migrate_boosted_task(p, env->src_cpu, env->dst_cpu))
 		return 0;
 
-#ifdef CONFIG_SCHED_WALT
-	if (p->wts.iowaited && is_min_capacity_cpu(env->dst_cpu) &&
+	if (p->in_iowait && is_min_capacity_cpu(env->dst_cpu) &&
 			!is_min_capacity_cpu(env->src_cpu))
 		return 0;
-#endif
 
 	if (!cpumask_test_cpu(env->dst_cpu, p->cpus_ptr)) {
 		int cpu;
